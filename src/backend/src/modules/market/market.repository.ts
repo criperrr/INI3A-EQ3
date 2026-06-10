825d69049e70e136179fbaba24a62c56a3f91d7f
heitor <heitor.utyama@unesp.br>
2026-06-10 01:19:59 -0300

    BE: bruh2

---
+++ src/backend/src/modules/market/market.repository.ts
@@ -0,0 +1,61 @@
+import { db } from '../../shared/database/database';
+import * as schema from '../../shared/database/schema';
+import { eq, sql } from "drizzle-orm";
+
+const Market = schema.market;
+
+export async function createMarket(market: CreateMarketDTO) {
+  return db.insert(Market).values(market).returning({
+    id: Market.id,
+    name: Market.name,
+    location: sql`${Market.location}`,
+  });
+}
+
+export async function  updateMarket(id: string | number, market: UpdateMarketDTO) {
+  return db.update(Market).set(market).returning({
+    name: Market.name,
+    email: Market.id,
+  }).where(eq(Market.id, Number(id)));
+}
+
+export async function deleteMarket(id: string | number) {
+  return (await db.delete(Market).where(eq(Market.id, Number(id)))).rowCount;
+}
+
+export async function getMarket(id: string | number) {
+  return db
+    .select({
+      id: Market.id,
+      name: Market.name,
+      location: sql`${Market.location}`,
+    })
+    .from(Market)
+    .where(eq(Market.id, Number(id)));
+}
+
+export async function getAllMarkets() {
+  return db
+    .select({
+      id: Market.id,
+      name: Market.name,
+      location: sql`${Market.location}`,
+    })
+    .from(Market);
+}
+
+export async function getRadiusLocationMarkets(coord: Point, radius: number) {
+  return db
+    .select({
+      id: Market.id,
+      name: Market.name,
+      location: sql`${Market.location}`,
+    })
+    .from(Market).where(sql`
+      ST_DWithin(
+      ST_SetSRID(${coord}, 4326),
+      ST_SetSRID(${Market.location}, 4326),
+      ${radius}
+      )
+      `);
+}
---
+++ src/backend/src/modules/me/me.service.ts
@@ -0,0 +1,50 @@
+import jwt from "jsonwebtoken";
+import { hash } from "../../shared/hash/bcrypt";
+import crypto from "crypto";
+import * as repository from "./me.repository";
+import {
+  DatabaseInternalError,
+  RedisInternalError,
+  NotFound,
+  ApiError,
+  parseDatabaseError
+} from "../../shared/errors/errors";
+
+
+export async function deleteSession(id: number) {
+  //Se o user não existe a api não retorna erro
+  try {
+    await repository.deleteUser(id)
+  }
+  catch (e) {
+    parseDatabaseError(e,"DATABASE: error on delete operation");
+  }
+}
+
+export async function updateSession(id: number, partialUser: UpdateUserDTO) {
+  try {
+    const { password, ...userRest } = partialUser;
+    userRest.passHash = await hash(password);
+
+
+    const user = (await repository.updateUser(id, userRest))[0];
+    if (!user) throw new NotFound("DATABASE: no user was updated.");
+    return user;
+  }
+  catch (e) {
+    if (e instanceof ApiError) throw e;
+    parseDatabaseError(e,"DATABASE: error on update operation");
+  }
+}
+
+export async function getMe(id: number) {
+  try {
+    const user = (await repository.getUser(id))[0];
+    if (!user) throw new NotFound("DATABASE: no user was returned.");
+    return user;
+  }
+  catch (e) {
+    if (e instanceof ApiError) throw e;
+    parseDatabaseError(e,"DATABASE: error on get operation");
+  }
+}
